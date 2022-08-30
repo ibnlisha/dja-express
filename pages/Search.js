69ca296c922052b0f0b4c4861643a02840bfa351
ibnlisha <ibn.lisha@yahoo.com>
2022-08-30 20:49:55 +0300

    added temporal search functionality

--- pages/Search.js
+++ pages/Search.js
@@ -1,19 +1,30 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import Product from '../components/Product'
 import { pdts } from '../public/SampleProducts'
+import styles from '../styles/Home.module.css'
 const Search = () => {
     const router = useRouter()
     const searchQuery = router.query.q
-    const escapeRegex = (text) =>
-        text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&") || 'No query string'
-    const regex = searchQuery? 
-    new RegExp(escapeRegex(searchQuery), 'gi')
-    : 'No search query'
+    // const escapeRegex = (text) =>
+    //     text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&") || 'No query string'
+    // const regex = searchQuery? 
+    // new RegExp(escapeRegex(searchQuery), 'gi')
+    // : 'No search query'
     // const found = pdts.filter(({name})=> regex.test(name))
     // const found = 'coffe macker'.match(regex)
     // console.log('found items', found)
+    const found = searchQuery ? pdts.filter(({name})=> name.toLowerCase()
+    .includes(searchQuery.toLowerCase())) : []
   return (
-    <div>{searchQuery}</div>
+    <div className={styles.container}>
+      {
+        !!found.length ? <div>
+          {found.map(product => <Product key = {product.id} {...product} />)}
+        </div>
+        : 'No products were found'
+      }
+    </div>
   )
 }
 
