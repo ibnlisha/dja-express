acaa5a6219cc32489f32106355f6b4b18c1c0bf9
ibnlisha <ibn.lisha@yahoo.com>
2022-08-31 16:58:08 +0300

    After adding resposivity

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,31 @@
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faPhone, faHandHolding } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faPhone, faHandHolding, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Navbar.module.css'
+import React ,{ useState } from 'react';
 const Navbar = () => {
+  const [showNavs, setShowNavs] = useState(true)
   return (
     <div className={styles.navbar}>
       <div className={styles.navContainer}>
-        <h1><Link href='/'>Dja Express</Link></h1>
-        <nav className={styles.nav}>
-            <li className={styles.navItem}><Link href="/services">
-            <span ><FontAwesomeIcon icon ={faHandHolding} />Services</span>
-                </Link></li>
-            <li className={styles.navItem}><Link href="/about">
-                <span><FontAwesomeIcon icon = {faUser} /> About us</span>
-                </Link></li>
-            <li className={styles.navItem}><Link href="/contact">
-                <span><FontAwesomeIcon icon ={faPhone} />Contact us</span>
-                </Link></li>
-        </nav>
+        <div className={styles.menuContainer}>
+          <h1><Link href='/'>Dja Express</Link></h1>
+              <button className={styles.menu} 
+              onClick ={() => setShowNavs(!showNavs)}>
+                {showNavs ? <FontAwesomeIcon icon={faBars}/> : <FontAwesomeIcon icon={faXmark}/>}
+              </button>
+            </div>
+          <nav className={showNavs ? styles.showNav : styles.hideNav}>
+              <li className={styles.navItem}><Link href="/services">
+              <span ><FontAwesomeIcon icon ={faHandHolding} />Services</span>
+                  </Link></li>
+              <li className={styles.navItem}><Link href="/about">
+                  <span><FontAwesomeIcon icon = {faUser} /> About us</span>
+                  </Link></li>
+              <li className={styles.navItem}><Link href="/contact">
+                  <span><FontAwesomeIcon icon ={faPhone} />Contact us</span>
+                  </Link></li>
+          </nav>
     </div>
     </div>
   )
--- components/Product.jsx
+++ components/Product.jsx
@@ -3,12 +3,14 @@ import Modal from "./Modal"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
+import styles from '../styles/Product.module.css'
+
 const Product = ({name, images}) => {
     const [show, setShow] = useState(false)
     return (
     <>
     {show && <Modal setShow={setShow} images = {images} />}
-        <button onClick ={e => setShow(true)} style={{border: 'none', width: '25%',}}>
+        <button onClick ={e => setShow(true)} className={styles.frame}>
             <div style={{width: '100%'}}>
                 <Image src = {images[0]} 
                 alt = {name} width='200' height='250'
